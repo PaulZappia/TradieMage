904214102171b1d7ada53280d92a5d6996e8c427
ScottBamsey <scott.bamsey@studytafensw.edu.au>
2025-03-14 16:26:52 +1100

    Messed with button sprite
    nothing changed

--- TradieMage/Assets/Scenes/Game Objects/NailButton.cs
+++ TradieMage/Assets/Scenes/Game Objects/NailButton.cs
@@ -5,12 +5,16 @@ public class NailButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private bool isPressed = false;
+    public SpriteRenderer buttonSprite;
+    public Sprite buttonActive;
+    public Sprite buttonInactive;
 
     public MovingPlatform connectedObject1;
+    //public GameObject connectedObject1;
 
     void Start()
     {
-        
+        buttonSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,7 +30,8 @@ public class NailButton : MonoBehaviour
         {
             //Debug.Log("yay");
             isPressed = true;
-            if(connectedObject1 != null)
+            buttonSprite.sprite.
+            if (connectedObject1 != null)
             {
                 connectedObject1.isActive = true;
             }
