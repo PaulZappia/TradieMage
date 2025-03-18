3b4647176b53f115bc3d188bfeaab4daffbb5754
Paul_Z <epicpaul5000@gmail.com>
2025-03-19 01:39:42 +1100

    Restructured Files
    Rearranged files to be more organised.

---
+++ TradieMage/Assets/2_Prefabs/2_Level Objects/LevelEndSequence.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndSequence : MonoBehaviour
+{
+    public string levelName;
+    [SerializeField] private bool isLevelFinished = false;
+    [SerializeField] private float endTimer = 5;
+    //public ParticleSystem = particles;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLevelFinished)
+        {
+            if (endTimer > 0)
+            {
+                endTimer -= Time.deltaTime;
+            }
+            else
+            {
+                SceneManager.LoadScene(levelName);
+            }
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isLevelFinished = true;
+            //ParticleSystem.Play();
+        }
+    }
+
+
+}
