1decd438177c91876665ee03d9ac5fe908f091c7
ScottBamsey <scott.bamsey@studytafensw.edu.au>
2025-05-29 16:17:45 +1000

    Started working on buttons to switch level groups.
    Mostly copied Paul's code, to adapt it to the buttons. I don't know how the events  system work.

---
+++ TradieMage/Assets/Z_Misc/LevelSelectionCanvas.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
+
+public class LevelSelectionCanvas : MonoBehaviour, IPointerDownHandler
+{
+
+    public Image leftButton;
+    public Image rightButton;
+
+
+    public Canvas canvas;
+    public Image transitionImage;
+    public RectTransform rect;
+
+    public float duration = 2.5f;
+    private float imgY = -540f;
+    private float imgStartX = 4607f; // 3623f
+    private float imgEndX = -2687f; // -1703f
+    private Vector2 startPos;
+    private Vector2 endPos;
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Start()
+    {
+        var buttons = GetComponentsInChildren<Image>();
+        leftButton = buttons[0];
+        //rightButton = buttons[1];
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        Debug.Log(eventData);//i am very confused and don't understand what's happening.
+        //SceneManager.LoadScene(levelDestination);
+        //StartCoroutine(LoadLevel());
+        //slide panel
+        StartCoroutine(AnimateTransition());
+
+    }
+
+    IEnumerator AnimateTransition()
+    {
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = time / duration;
+
+            // Ease-in
+            float easedT = Mathf.SmoothStep(0f, 1f, t);
+            rect.anchoredPosition = Vector2.Lerp(startPos, endPos, easedT);
+            //transitionImage.transform.position = Vector2.Lerp(startPos, endPos, easedT);
+
+            yield return null;
+        }
+
+        // Set at final position just to make sure dawg
+        rect.anchoredPosition = endPos;
+        //transitionImage.transform.position = endPos;
+        Destroy(this.gameObject);
+        yield return null;
+    }
+
+
+}
