650f1859bf7a4ad7aa9dfb1d41521dc1d6328957
ScottBamsey <scott.bamsey@studytafensw.edu.au>
2025-06-05 13:41:31 +1000

    Added warble sfx to range extender
    Needs some fade out

--- TradieMage/Assets/2_Prefabs/2_Level Objects/RangeExtender.cs
+++ TradieMage/Assets/2_Prefabs/2_Level Objects/RangeExtender.cs
@@ -1,16 +1,31 @@
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 public class RangeExtender : MonoBehaviour
 {
 
     public ParticleSystem ParticleEffect;
     private bool isPlayerIn = false;
 
+    public AudioSource audioSource;
+    // Changed to support multiple jump sounds
+    public AudioClip warbleSound;
+    [UnityEngine.Range(0f, 1f)]
+    public float sfxVolume = 0.7f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Get or add AudioSource component
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+        }
     }
     // Update is called once per frame
     void Update()
@@ -20,10 +35,12 @@ public class RangeExtender : MonoBehaviour
             if (!ParticleEffect.isPlaying) {
                 ParticleEffect.Play();
             }
+            PlaySound();
         }
         else
         {
             ParticleEffect.Stop();
+            audioSource.Stop();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -51,4 +68,22 @@ public class RangeExtender : MonoBehaviour
 
         }
     }
+
+
+    private void PlaySound()
+    {
+        // Check if there are any jump sounds and audio source is assigned
+        if (warbleSound != null && audioSource != null)
+        {
+            // Select a random jump sound from the array
+            //AudioClip randomJumpSound = warbleSound;
+            audioSource.PlayOneShot(warbleSound);
+        }
+        else
+        {
+            Debug.LogWarning("No wable sounds assigned or audio source missing!");
+        }
+    }
+
+
 }
